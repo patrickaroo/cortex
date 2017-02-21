d182532c538cfaed2e9e9aab4701edfa3c38ad30
Harold <hhausman@gmail.com>
2017-02-21 11:52:49 -0700

    [suite/logistic-test] Idealized version of this test.
    Doesn't work, obviously.
    
    Imagines a world where there's a simple `nn` pass-through interface to everything needed to train a basic net.

--- test/clj/cortex/suite/logistic_test.clj
+++ test/clj/cortex/suite/logistic_test.clj
@@ -1,46 +1,36 @@
 (ns cortex.suite.logistic-test
-  (:require [clojure.java.io :as io]
+  (:require [clojure.test :refer :all]
             [clojure.string :as s]
-            [clojure.test :refer :all]
-            [cortex.dataset :as dataset]
-            [cortex.nn.layers :as layers]
-            [cortex.nn.network :as network]
-            [cortex.nn.traverse :as traverse]
-            [cortex.suite.train :as train]
-            [cortex.suite.inference :as inference]))
+            [cortex.nn.core :as nn]))
 
-;; This test namespace uses the logistic classifier to learn to predict whether
-;; people will default based on their balance and income.
+;; This test does logistic regression to learn to predict whether people will
+;; default based on their balance and income.
 
-;; "Default" dataset taken from: https://cran.r-project.org/web/packages/ISLR/index.html
+;; "Default" dataset: https://cran.r-project.org/web/packages/ISLR/index.html
 ;; Distributed under the GPL-2 LICENSE
-;; The format is USER_ID,STUDENT?,DEFAULT?,BALANCE,INCOME
+;; The columns are USER_ID,STUDENT?,DEFAULT?,BALANCE,INCOME
 
 (defn default-dataset
+  ".csv -> sequence of maps: Could use a real csv parser but doesn't."
   []
   (->> "test/data/default.csv"
       (slurp)
       (s/split-lines)
-      (rest)                                     ;; ignore the header row
-      (map (fn [l] (drop 2 (s/split l #"," ))))  ;; ignore id, student cols
+      (rest) ;; ignore the header row
+      (map (fn [l] (drop 2 (s/split l #"," )))) ;; ignore id, student columns
       (mapv (fn [[default balance income]]
              {:data [(Double. balance) (Double. income)]
               :labels (if (= default "\"Yes\"") [1.0] [0.0])}))))
 
-(def description
-  [(layers/input 2)
-   (layers/batch-normalization)
-   (layers/linear->logistic 1)])
+(def network
+  [(nn/input 2)
+   (nn/batch-normalization)
+   (nn/linear->logistic 1)])
 
 (deftest logistic-test
-  (io/delete-file "trained-network.nippy" true)
-  (let [default-data (default-dataset)
-        inf-data (repeatedly #(rand-nth default-data))
-        dataset (dataset/map-sequence->dataset inf-data (/ (count default-data) 20))
-        network (-> description network/build-network traverse/auto-bind-io)
-        _ (train/train-n dataset description network :batch-size 50 :epoch-count 300 :simple-loss-print? true)
-        trained-network (train/load-network "trained-network.nippy" description)
-        [[should-def] [shouldnt-def]] (inference/infer-n-observations trained-network [[5000.0 10.0] [5.0 100000.0]]
-                                                                      2 :batch-size 2)]
+  (nn/reset-training) ;; would delete any cached trained-network files
+  (let [dataset (nn/map-sequence->batched-dataset (default-dataset) :observations-per-epoch 500)
+        trained-network (nn/train dataset network :batch-size 50 :epoch-count 300)
+        [[should-def] [shouldnt-def]] (nn/infer trained-network [[5000.0 10.0] [5.0 100000.0]] :batch-size 2)]
     (is (> should-def 0.97))
     (is (< shouldnt-def 0.02))))
