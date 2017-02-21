1685cfde36dbff283a2d2c59fc3179d5c8fc70d5
bburdette <bburdette@gmail.com>
2017-02-21 11:04:06 -0700

    locally tested script

--- scripts/build-and-deploy-docs.sh
+++ scripts/build-and-deploy-docs.sh
@@ -2,18 +2,33 @@
 
 set -e
 
+# udpate submodules, including cortex.docs
+git submodule init
+git submodule update --remote --merge
+
+# make sure we're on master.
+cd ./cortex.docs
+git checkout master
+#git fetch
+#git merge origin/master
+
 # generate docs
+cd ..
 lein codox
 
 # remove old docs.
-rm -f ../codox.docs/*html
-rm -rf ../codox.docs/css
-rm -rf ../codox.docs/js
+rm -f ./cortex.docs/*html
+rm -rf ./cortex.docs/css
+rm -rf ./cortex.docs/js
 
 #copy in the new stuff.
-cp -r ../target/doc/* ../cortex.docs
+cp -r ./target/doc/* ./cortex.docs
 
 # commit to the cortex.docs repo/submodule
+cd ./cortex.docs
 git add -u
+git add *
+git add css/*
+git add js/*
 git commit -m "update cortex docs"
 git push
